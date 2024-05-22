f64ef802e093ec85cee664bf81d1bc6741bc48cc
Zeworxx <zeworxx@gmail.com>
2024-05-22 10:49:55 +0200

    add dbconfiig

--- src/infrastructure/database/database.module.ts
+++ src/infrastructure/database/database.module.ts
@@ -1,4 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({})
+@Module({
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: process.env.DATABASE_HOST,
+            port: parseInt(process.env.DATABASE_PORT),
+            username: process.env.DATABASE_USER,
+            password: process.env.DATABASE_PASSWORD,
+            database: process.env.DATABASE_NAME,
+            entities: [],
+            // Synchronize should be set to false in production
+            synchronize: true,
+          })
+      ],
+})
 export class DatabaseModule {}
